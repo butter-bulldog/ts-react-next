721270ae3d7aa210c940d2c1f0baa065a2a500fd
FumiakiSaito <haratairani3000points@gmail.com>
2023-02-08 23:12:11 +0900

    add layout

--- src/services/auth/signin.ts
+++ src/services/auth/signin.ts
@@ -1,5 +1,5 @@
-import { ApiContext, User } from "../../types/data"
-import { fetcher } from "../../utils"
+import { ApiContext, User } from "types/data"
+import { fetcher } from "utils"
 
 export type SigninParams = {
   /**
