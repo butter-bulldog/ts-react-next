ff862a48ec7885bfe400cc34d32cf8bba84da07a
FumiakiSaito <haratairani3000points@gmail.com>
2023-02-23 00:17:55 +0900

    fix typo eslintrc.json

---
+++ src/containers/AddToCartButtonContainer.tsx
@@ -0,0 +1,43 @@
+import Button from "components/atoms/Button"
+import {useShoppingCartContext} from 'contexts/ShoppingCartContext'
+import type {Product} from 'types'
+
+interface AddToCartButtonContainerProps {
+  product: Product
+  onAddToCartButtonClick?: (product: Product) => void
+}
+
+/**
+ * カート追加ボタンコンテナ
+ */
+const AddToCartButtonContainer = ({
+  product,
+  onAddToCartButtonClick,
+}: AddToCartButtonContainerProps) => {
+  const {cart, addProductToCart} = useShoppingCartContext()
+
+  const handleAddToCartButtonClick = () => {
+    const productId = Number(product.id)
+    const result = cart.findIndex((v) => v.id === productId)
+
+    if (result === -1) {
+      addProductToCart(product)
+    }
+
+    onAddToCartButtonClick && onAddToCartButtonClick(product)
+
+  }
+
+  return (
+    <Button
+      width={{base: '100%', md: '400px'}}
+      height="66px"
+      onClick={handleAddToCartButtonClick}
+    >
+      カートに追加
+    </Button>
+  )
+
+}
+
+export default AddToCartButtonContainer
