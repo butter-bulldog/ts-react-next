bab09a71510230fb8f6b2c9d0ee73a9be8c20c42
FumiakiSaito <haratairani3000points@gmail.com>
2023-02-23 01:21:23 +0900

    hoge

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -8,3 +8,39 @@ import Box from 'components/layout/Box'
 import Flex from 'components/layout/Flex'
 import Breadcrumb from 'components/molecules/Breadcrumb'
 import Layout from 'components/templates/Layout'
+
+const CartPage: Nextpage = () => {
+  // 認証ガード
+  useAuthGuard()
+
+  return (
+    <Layout>
+      <Flex
+        paddingTop={2}
+        paddingBottom={2}
+        paddingLeft={{ base: 2, md: 0 }}
+        paddingRight={{ base: 2, md: 0 }}
+        justifyContent="center"
+      >
+        <Box width="1240px">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link href="/">
+                <a>トップ</a>
+              </Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>カート</BreadcrumbItem>
+          </Breadcrumb>
+          <Box>
+            <Text display="block" variant="large" as="h1">
+              カート
+            </Text>
+            <CartContainer />
+          </Box>
+        </Box>
+      </Flex>
+    </Layout>
+  )
+}
+
+export default CartPage
